413c22738f4204ebf037b2e2e55267e239c2b63a
Daniele Denti <denti.daniele@gmail.com>
2012-10-27 21:36:36 +0200

    reprepro step 1

---
+++ reprepro/configure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# name        : configure.sh
+# description : reprepro toolbox configuration script example.
+#
+# 2010-01-27 daniele.denti@bluewin.ch 1.0
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+SCRIPT_FOLDER=`dirname $0`
+
+#
+# MAIN
+#
+
+. ${SCRIPT_FOLDER}/conflib
+. /lib/lsb/init-functions
+
+# do not run as root
+if [ "`whoami`" != "root" ]; then
+ log_failure_msg "You have to be root to run this script ! `whoami`"
+ exit 1
+fi
+
+reprepro
